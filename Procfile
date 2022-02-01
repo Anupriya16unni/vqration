b509b4d60407e1b0011c97bafcf3fa7a864f2991
Anupriya16unni <oanupriya16@gmail.com>
2022-02-01 11:58:37 +0530

    vqration

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vqrationprjct.wsgi --log-file -
---
+++ vqrationapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VqrationappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vqrationapp'
